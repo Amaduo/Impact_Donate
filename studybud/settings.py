eb39528156d50a7a5f11dda412607ac1c4dd2af9
adul <amaduoadul555@gmail.com>
2025-03-30 16:37:22 +0300

    import os and port

--- studybud/settings.py
+++ studybud/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+PORT = os.getenv("PORT", "8000")  # Default to 8000 if PORT is not set
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
